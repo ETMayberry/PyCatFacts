cba216ed817d5dcf643f086abcc94bc41ddd607d
Eric Mayberry <ETMayberry@users.noreply.github.com>
2018-06-16 09:29:19 -0400

    added catfacts_agent.ps1

---
+++ catfacts_agent.ps1
@@ -0,0 +1,8 @@
+﻿function speak {
+    Add-Type -AssemblyName System.Speech
+
+    $Catfact = Invoke-WebRequest -Uri 'http://127.0.0.1/fact' -UseBasicParsing | Select-Object -ExpandProperty 'Content'
+
+    $Speaker = New-Object System.Speech.Synthesis.SpeechSynthesizer;
+    $Speaker.Speak($Catfact);
+}
