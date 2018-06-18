5c2360e9f6b7ce13609b00217596c34b441c035b
Muffin_Button <brooke.pentest@gmail.com>
2018-06-17 20:18:56 -0400

    added reference

--- catfacts_agent.ps1
+++ catfacts_agent.ps1
@@ -1,4 +1,5 @@
-﻿function speak {
+﻿# reference: https://gist.github.com/johnallers/178576f097a8a7986fcb17a92c88a486 
+function speak {
     Add-Type -AssemblyName System.Speech
 
     $Catfact = Invoke-WebRequest -Uri 'http://127.0.0.1/fact' -UseBasicParsing | Select-Object -ExpandProperty 'Content'
